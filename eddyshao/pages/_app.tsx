9e27f837feb8a752a85189474cc141bf204a559f
Eddy Shao <dev@eddyshao.com>
2023-05-01 07:16:51 +1000

    add google analytics

--- eddyshao/pages/_app.tsx
+++ eddyshao/pages/_app.tsx
@@ -1,12 +1,26 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { Rubik } from "@next/font/google";
+import Script from "next/script";
 
 const rubik = Rubik({ subsets: ["latin"] });
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <main className={rubik.className}>
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}`}
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', '${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}');
+        `}
+      </Script>
       <Component {...pageProps} />
     </main>
   );
